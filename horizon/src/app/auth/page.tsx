5c50f71d2cdd20b7049196be3254dc410a7fefe3
Ashish <ashish061104@gmail.com>
2026-01-21 19:31:18 +0530

    note shadow + dev card size fix

--- horizon/src/app/auth/page.tsx
+++ horizon/src/app/auth/page.tsx
@@ -62,11 +62,12 @@ export default function AuthBox() {
         width={250}
         height={250}
         priority
-        className="absolute top-18 right-6 -rotate-4"
+        className="absolute top-18 right-6 -rotate-4
+    w-40 sm:w-56 md:w-60 h-auto drop-shadow-xl"
       />
 
       {/* Centered login/signup */}
-      <div className="flex justify-center items-center min-h-screen">
+      <div className="flex justify-center items-center min-h-screen drop-shadow-2xl">
         <div className="relative z-10 bg-white p-8 rounded-xl shadow-md w-96">
           <div className="flex justify-between items-center mb-10">
             <h2 className="text-2xl text-black font-bold">
@@ -100,8 +101,8 @@ export default function AuthBox() {
             onClick={handleAuth}
             disabled={loading}
             className={`w-full py-2 rounded ${loading
-                ? "bg-gray-900 text-white"
-                : "bg-black hover:bg-white hover:text-black hover:border"
+              ? "bg-gray-900 text-white"
+              : "bg-black hover:bg-white hover:text-black hover:border"
               }`}
           >
             {loading ? "Please wait..." : isLogin ? "Login" : "Sign Up"}
